8315966ba1769b371a9cfd76b3d675ae77181925
HHHLZX <138979515+HHHLZX@users.noreply.github.com>
2024-06-22 21:46:13 +0800

    添加跳转登录页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,50 @@
 // index.js
 Page({
-    data:{
-        "movieList":[]
-    },
-    onLoad:function(){
-        console.log("页面加载成功");
+  data: {
+    movieList: [],
+    groupedMovieList: [] // 用于存储分组后的数据  
+  },
+  onLoad: function () {
+    this.groupMovies();
+  },
+  groupMovies: function () {
+    let _this = this;
+    wx.request({
+      url: 'http://localhost:8080/api/movie/list',
+      method: "GET",
+      success: function (res) {
+        let movies = res.data.data;
+        _this.setData({
+          movieList: movies
+        });
 
-        let _this = this
+        // 将分组逻辑移动到这里  
+        let groupedMovieList = _this.data.movieList.reduce((accumulator, currentValue, currentIndex) => {
+          let chunkIndex = Math.floor(currentIndex / 3); // 计算当前项应该属于哪个分组  
 
-        wx.request({
-          url: 'http://localhost:8080/api/movie/list',
-          method:"GET",
-          success:function(res){
-              console.log(res);
-              let d = res.data.data
-              _this.setData({movieList:d})
+          if (!accumulator[chunkIndex]) {
+            accumulator[chunkIndex] = [];
           }
-        })
-    }
+
+          accumulator[chunkIndex].push(currentValue);
+
+          return accumulator;
+        }, []);
+
+        // 更新页面数据  
+        _this.setData({
+          groupedMovieList: groupedMovieList
+        });
+      }
+    });
+  },
+  // 跳转登录界面
+  navigateToLogin: function () {
+    // wx.redirectTo({
+    //   url: "/pages/login/login"
+    // })
+    wx.navigateTo({
+      url: '/pages/login/login'
+    });
+  }
 })
